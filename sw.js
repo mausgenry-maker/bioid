47fb1bd48fbd686010a50c8575409bcfb9237f6e
mausgenry-maker <mausgenry@gmail.com>
2026-04-25 17:31:04 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE_NAME = 'bioid-v1';
-const ASSETS = ['./index.html']; // добавь сюда иконки, если есть
+const CACHE_NAME = 'bioid-v5';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/dist/face-api.min.js',
+  'models/tiny_face_detector_model-weights_manifest.json',
+  'models/tiny_face_detector_model-shard1',
+  'models/face_landmark_68_model-weights_manifest.json',
+  'models/face_landmark_68_model-shard1',
+  'models/face_recognition_model-weights_manifest.json',
+  'models/face_recognition_model-shard1'
+];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
